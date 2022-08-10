ba9dbe9ff27968e1716e00b932220f5ff0a046e9
Alison <alisson_gamer2000@outlook.com>
2022-08-10 15:04:53 -0300

    add Exception in program Chess

--- XadrezCSharp/GameBoard/Tabuleiro.cs
+++ XadrezCSharp/GameBoard/Tabuleiro.cs
@@ -24,6 +24,23 @@ namespace GameBoard
 
         }
 
+        //Fazendo um sobrecarga
+        public Peca PecaMth(Posicao position) {
+
+            return Pecas[position.Linha, position.Coluna];
+
+        }
+
+        //verificando se tem uma peca na posicao
+        public bool ExistePeca(Posicao position) 
+        {
+            //usando o method validarPosicao antes
+            ValidarPosicao(position);
+
+            //se tiver certo ele vem aqui depois e verifica se tem uma peca na posicao
+            return PecaMth(position) != null;
+        }
+
         public void ColocarPeca(Peca p, Posicao position) 
         {
             //falando que eu quero a pecas que esta na posicao linha E coluna
@@ -32,6 +49,32 @@ namespace GameBoard
             //aqui estou dizendo agora a posicao dessa peça é posistion
             p.Posicao = position;
 
+        }
+
+        //testando posicao
+        public bool PosicaoValida(Posicao position) 
+        {
+            //variavel local 
+            bool valido = true;
+
+            //verificando se  a posicao é maior que 0 e menor que 7
+            if (position.Linha < 0 || position.Linha >= Linhas 
+                || position.Coluna < 0 || position.Coluna >= Colunas) 
+            {
+                valido = false;
+            }
+
+            //vai retorna true se o if for falso
+            return valido;
+        }
+
+        public void ValidarPosicao(Posicao position) 
+        {
+            //Verificando se não for valida o movimento
+            if (!PosicaoValida(position)) 
+            {
+                throw new TabuleiroException("Posição Invalida!!");
+            }
         }
     }
 }
---
+++ XadrezCSharp/GameBoard/TabuleiroException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GameBoard
+{
+    internal class TabuleiroException : Exception
+    {
+        //Execação do tabuleiro
+        public TabuleiroException(string msg) : base(msg) 
+        {
+            
+        } 
+    }
+}
